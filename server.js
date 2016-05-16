7313f89135dee7a70c81442a2a0450e0450ae8a5
Матвей Кравцов <matvey@Mac-mini-Matvej.local>
2016-05-16 21:27:28 +0300

    some changes

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+'use strict';
+
 var express = require('express'),
     app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-const PORT=8000; 
-app.listen(PORT, function(){
-    console.log("Server listening on: http://localhost:%s", PORT);
+const PORT=80; 
+
+var server = app.listen(PORT, function(){
+    //console.log("Server listening on: http://localhost:%s", PORT);
+    console.log('App listening at http://%s:%s', server.address().address,
+    server.address().port);
+  console.log('Press Ctrl+C to quit.');
 });
