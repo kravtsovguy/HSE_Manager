c0471a08b0d1d784d39be3b02e728a1c8c8e1458
Матвей Кравцов <matvey@Mac-mini-Matvej.local>
2016-05-17 00:33:00 +0300

    ...

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
 
 const PORT=80; 
 
-var server = app.listen(PORT, function(){
+var server = app.listen(process.env.PORT || 8080,'0.0.0.0', function(){
     //console.log("Server listening on: http://localhost:%s", PORT);
     console.log('App listening at http://%s:%s', server.address().address,
     server.address().port);
